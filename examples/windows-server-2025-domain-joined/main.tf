64b887e219cdbd0a28d5b393ce8dcdccbdfb7796
eladzazon <eladzazon@gmail.com>
2026-03-17 14:35:14 +0200

    Fix: Automated Windows OOBE in domain-joined example to prevent getting stuck on setup page

--- examples/windows-server-2025-domain-joined/main.tf
+++ examples/windows-server-2025-domain-joined/main.tf
@@ -57,6 +57,40 @@ locals {
           </Identification>
         </component>
       </settings>
+      <settings pass="oobeSystem">
+        <component name="Microsoft-Windows-Shell-Setup"
+                   processorArchitecture="amd64"
+                   publicKeyToken="31bf3856ad364e35"
+                   language="neutral"
+                   versionScope="nonSxS"
+                   xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State">
+          <UserAccounts>
+            <AdministratorPassword>
+              <Value>${var.domain_password}</Value>
+              <PlainText>true</PlainText>
+            </AdministratorPassword>
+          </UserAccounts>
+          <OOBE>
+            <HideEULAPage>true</HideEULAPage>
+            <HideLocalUserAccountScreen>true</HideLocalUserAccountScreen>
+            <HideOnlineAccountScreens>true</HideOnlineAccountScreens>
+            <HideWirelessSetupInOOBE>true</HideWirelessSetupInOOBE>
+            <NetworkLocation>Work</NetworkLocation>
+            <ProtectYourPC>1</ProtectYourPC>
+          </OOBE>
+        </component>
+        <component name="Microsoft-Windows-International-Core"
+                   processorArchitecture="amd64"
+                   publicKeyToken="31bf3856ad364e35"
+                   language="neutral"
+                   versionScope="nonSxS"
+                   xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State">
+          <InputLocale>en-US</InputLocale>
+          <SystemLocale>en-US</SystemLocale>
+          <UILanguage>en-US</UILanguage>
+          <UserLocale>en-US</UserLocale>
+        </component>
+      </settings>
     </unattend>
   XML
 }
